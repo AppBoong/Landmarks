859e6d7c498edef158f312510d9807471d85c8ff
JaeYeol Lee <powerhotdog21@gmail.com>
2022-07-01 01:05:47 +0900

    Initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jaeyeol on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
