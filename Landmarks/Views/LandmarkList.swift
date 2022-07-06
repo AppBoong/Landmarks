a69d8590ca1849123994be5b393ba30b0ea66fc1
JaeYeol Lee <powerhotdog21@gmail.com>
2022-07-07 01:25:07 +0900

    session2_section4

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Jaeyeol on 2022/07/07.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
